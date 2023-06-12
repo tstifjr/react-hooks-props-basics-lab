9048d55221ce684e91aaa555fb874fb313fca74e
TJ Stifter <tjstifter@gmail.com>
2023-06-12 14:58:25 -0400

    completes lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,18 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+
+      function About(props) {
+        console.log(props.links)
   return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
+      <div id="about">
+        <h2>About Me</h2>
+        {props.bio ? <p>{props.bio}</p> : null}
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        {/* add your <Links /> component here */}
+        <Links github={props.links.github} linkedin={props.links.linkedin}/>
+      </div>
+      );
 }
 
-export default About;
+      export default About;
